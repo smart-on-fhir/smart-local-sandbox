3005a5881e46a988399908e622257562db187ef6
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2018-04-12 14:17:42 -0400

    Use the public launcher

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
-git subtree add --prefix=ext/smart-launcher        https://github.com/chb/smart-launcher.git                  master --squash
+git subtree add --prefix=ext/smart-launcher        https://github.com/smart-on-fhir/smart-launcher.git        master --squash
 git subtree add --prefix=ext/generated-sample-data https://github.com/smart-on-fhir/generated-sample-data.git master --squash
 git subtree add --prefix=ext/tag-uploader          https://github.com/smart-on-fhir/tag-uploader.git          master --squash
 git subtree add --prefix=ext/xml-bundle-uploader   https://github.com/smart-on-fhir/xml-bundle-uploader.git   master --squash
@@ -8,7 +8,7 @@ git subtree add --prefix=ext/fhir-viewer           https://github.com/smart-on-f
 git subtree pull -P ext/fhir-viewer                https://github.com/smart-on-fhir/fhir-viewer.git           master --squash
 git subtree pull -P ext/generated-sample-data      https://github.com/smart-on-fhir/generated-sample-data.git master --squash
 git subtree pull -P ext/xml-bundle-uploader        https://github.com/smart-on-fhir/xml-bundle-uploader.git   master --squash
-git subtree pull -P ext/smart-launcher             https://github.com/chb/smart-launcher.git                  master --squash
+git subtree pull -P ext/smart-launcher             https://github.com/smart-on-fhir/smart-launcher.git        master --squash
 git subtree pull -P ext/tag-uploader               https://github.com/smart-on-fhir/tag-uploader.git          master --squash
 git subtree pull -P ext/synthea                    https://github.com/synthetichealth/synthea.git             master --squash
 
